e66802f5a391eb65e921beed187fdf51393ca69c
Faye Rosenshein <rosefaye.a@gmail.com>
2022-12-06 15:34:12 -0700

    feat: Add code to apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,50 @@
 
 console.log('I will be a fetch request!')
 
+//fetch 3 times, get data, then call New UserRepository 
+// fetch('https://example.com/', {
+//   method: 'GET',
+//   body: new FormData(),
+// });
+//in script, function loadData() => calls the fetches from apiCalls.js, then creates new UserRepository
+
+//const user = new User()
+// fetch('https://opentdb.com/api.php?amount=20&category=22')
+//     .then((response) => (response.json()))
+//     .then((data) => {
+//         data.results.forEach(results => console.log(results.correct_answer))
+//     })
+let userData
+let sleepData
+let hydroData
+
+function loadUserData() {
+    const userURL = 'https://fitlit-api.herokuapp.com/api/v1/users'
+    return fetch(userURL)
+    .then((response) => response.json())
+    .then((data) => {
+        userData = data
+        console.log(userData)
+    })
+}
+function loadSleepData() {
+    const sleepURL = 'https://fitlit-api.herokuapp.com/api/v1/sleep'
+    return fetch(sleepURL)
+        .then((response) => response.json())
+        .then((data) => {
+            sleepData = data
+            console.log(sleepData)
+        })
+}
+function loadHydrationData() {
+    const hydrationURL = 'https://fitlit-api.herokuapp.com/api/v1/hydration'
+    return fetch(hydrationURL)
+        .then((response) => response.json())
+        .then((data) => {
+            hydroData = data
+            console.log(hydroData)
+        })
+}
+
+// export default apiCalls;
+export default {loadUserData, loadSleepData, loadHydrationData};
--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,9 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-console.log(userData,"<>>>>userData")
+// console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
-import './css/styles.css';
+import './html-css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
@@ -12,6 +12,24 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 // An example of how you tell webpack to use a JS file
 
-import userData from './data/users';
+const userPromise = apiCalls.loadUserData()
+const sleepPromise = apiCalls.loadSleepData()
+const hydrationPromise = apiCalls.loadHydrationData()
 
+
+Promise.all([userPromise, sleepPromise, hydrationPromise])
+    .then((values) => {
+        {
+            console.log(values);
+        }
+})
+
+// console.log(userRepo)
+// apiCalls.loadUsers(userRepo)
+
+// import userData from './data/users';
+import apiCalls from './apiCalls';
 import UserRepository from './UserRepository';
+// import hydrationTestData from '../test/hydration-test-data';
+// import sleepTestData from '../test/sleep-test-data';
+
