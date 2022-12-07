a967a196546876959cbd455db1ca0ad1253e16b8
Faye Rosenshein <rosefaye.a@gmail.com>
2022-12-06 17:21:57 -0700

    feat: Add some functionality to script.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,21 +1,7 @@
 // Your fetch requests will live here!
 
-
 console.log('I will be a fetch request!')
 
-//fetch 3 times, get data, then call New UserRepository 
-// fetch('https://example.com/', {
-//   method: 'GET',
-//   body: new FormData(),
-// });
-//in script, function loadData() => calls the fetches from apiCalls.js, then creates new UserRepository
-
-//const user = new User()
-// fetch('https://opentdb.com/api.php?amount=20&category=22')
-//     .then((response) => (response.json()))
-//     .then((data) => {
-//         data.results.forEach(results => console.log(results.correct_answer))
-//     })
 let userData
 let sleepData
 let hydroData
@@ -23,11 +9,11 @@ let hydroData
 function loadUserData() {
     const userURL = 'https://fitlit-api.herokuapp.com/api/v1/users'
     return fetch(userURL)
-    .then((response) => response.json())
-    .then((data) => {
-        userData = data
-        console.log(userData)
-    })
+        .then((response) => response.json())
+        .then((data) => {
+            userData = data
+            return userData
+        })
 }
 function loadSleepData() {
     const sleepURL = 'https://fitlit-api.herokuapp.com/api/v1/sleep'
@@ -35,7 +21,7 @@ function loadSleepData() {
         .then((response) => response.json())
         .then((data) => {
             sleepData = data
-            console.log(sleepData)
+            return sleepData
         })
 }
 function loadHydrationData() {
@@ -44,9 +30,7 @@ function loadHydrationData() {
         .then((response) => response.json())
         .then((data) => {
             hydroData = data
-            console.log(hydroData)
+            return hydroData
         })
 }
-
-// export default apiCalls;
-export default {loadUserData, loadSleepData, loadHydrationData};
+export default { loadUserData, loadSleepData, loadHydrationData };
--- src/scripts.js
+++ src/scripts.js
@@ -11,18 +11,21 @@ import './images/turing-logo.png'
 console.log('This is the JavaScript entry file - your code begins here.');
 
 // An example of how you tell webpack to use a JS file
-
+const header1 = document.querySelector('h1')
 const userPromise = apiCalls.loadUserData()
-const sleepPromise = apiCalls.loadSleepData()
 const hydrationPromise = apiCalls.loadHydrationData()
+const sleepPromise = apiCalls.loadSleepData()
 
-
-Promise.all([userPromise, sleepPromise, hydrationPromise])
+Promise.all([userPromise, hydrationPromise, sleepPromise])
     .then((values) => {
         {
-            console.log(values);
+            let userRepo = new UserRepository(values[0],values[1],values[2])
+            console.log(userRepo)
+            console.log('script: ',values[0].userData[0].id);
+            header1.innerText = values[0].userData[0].name
         }
 })
+
 
 // console.log(userRepo)
 // apiCalls.loadUsers(userRepo)
